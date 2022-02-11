8248188140b30238c2fef1418cfc36db53be5b0c
Austin  Marrazza <austinmarrazza@Austins-MacBook-Pro.local>
2022-02-11 12:54:40 -0800

    wip

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,22 @@
 // Write your "projects" router here!
+const express = require('express');
+
+const Projects = require('./projects-model');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Projects.get()
+        .then(resp => {
+            res.json(resp);
+        }).catch()
+})
+
+router.use((err, req, res, next) => { //eslint-disable-line
+    res.status(err.status || 500).json({
+        customMessage: 'An error occurred',
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const projectsRouter = require('./projects/projects-router')
 const server = express();
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+server.use(express.json());
+server.use('/api/projects', projectsRouter);
+
+
+server.get('/', (req, res) => {
+    res.send(`<h1>gm</h1>`);
+});
 
 module.exports = server;
